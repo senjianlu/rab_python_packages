cefed2b90538193fdc80be9453e6c13746e18d4c
Rabbir <m18018230731@163.com>
2021-11-03 10:40:31 +0800

    feat: 为 rab_ip 模块新增两个 IP 信息获取接口

--- rab_ip.py
+++ rab_ip.py
@@ -22,6 +22,33 @@ r_logger = rab_logging.r_logger()
 
 
 """
+@description: 解析 IP 接口返回的数据
+-------
+@param:
+-------
+@return:
+"""
+def parse_ip_info(api_url, r_json):
+    ip_info = {
+        "ip": None,
+        "location": None
+    }
+    if (api_url == "http://ip-api.com/json/?lang=zh-CN"):
+        ip_info["ip"] = r_json["query"] if "query" in r_json else None
+        ip_info["location"] = r_json["country"] if "country" in r_json else None
+    elif(api_url == "https://www.ip.cn/api/index?ip=&type=0"):
+        ip_info["ip"] = r_json["ip"] if "ip" in r_json else None
+        ip_info["location"] = r_json["address"] if "address" in r_json else None
+    elif(api_url == "https://ip-api.io/json"):
+        ip_info["ip"] = r_json["ip"] if "ip" in r_json else None
+        ip_info["location"] = r_json["country_name"] \
+            if "country_name" in r_json else None
+    elif(api_url == "https://api.ipify.org/?format=json"):
+        ip_info["ip"] = r_json["ip"] if "ip" in r_json else None
+        ip_info["location"] = None
+    return ip_info
+
+"""
 @description: 获取 IP 信息
 -------
 @param:
@@ -31,36 +58,24 @@ r_logger = rab_logging.r_logger()
 def get_ip_info(proxies=None,
                 timeout=int(rab_config.load_package_config(
                     "rab_config.ini", "rab_requests", "timeout"))):
-    # 接口：http://ip-api.com/json/?lang=zh-CN
-    try:
-        r = requests.get("http://ip-api.com/json/?lang=zh-CN",
-                         proxies=proxies,
-                         timeout=timeout)
-        r_json = json.loads(r.text)
-        ip = r_json["query"] if "query" in r_json else None
-        location = r_json["country"] if "country" in r_json else None
-        return {
-            "ip": ip,
-            "location": location
-        }
-    except Exception as e:
-        r_logger.error("使用 ip-api.com 获取 IP 信息出错！")
-        r_logger.error(e)
-    # 接口：https://www.ip.cn/api/index?ip=&type=0
-    try:
-        r = requests.get("https://www.ip.cn/api/index?ip=&type=0",
-                         proxies=proxies,
-                         timeout=timeout)
-        r_json = json.loads(r.text)
-        ip = r_json["ip"] if "ip" in r_json else None
-        location = r_json["address"] if "address" in r_json else None
-        return {
-            "ip": ip,
-            "location": location
-        }
-    except Exception as e:
-        r_logger.error("使用 ip.cn 获取 IP 信息出错！")
-        r_logger.error(e)
+    api_urls = [
+        "http://ip-api.com/json/?lang=zh-CN",
+        "https://www.ip.cn/api/index?ip=&type=0",
+        "https://ip-api.io/json",
+        # 只会返回 IP 的接口
+        "https://api.ipify.org/?format=json"
+    ]
+    for api_url in api_urls:
+        try:
+            r = requests.get(api_url, proxies=proxies, timeout=timeout)
+            r_json = json.loads(r.text)
+            ip_info = parse_ip_info(api_url, r_json)
+            # 如果访问成功了有 IP 信息了则退出
+            if (ip_info["ip"] or ip_info["location"]):
+                return ip_info
+        except Exception as e:
+            r_logger.error("使用 {} 获取 IP 信息出错！".format(api_url))
+            r_logger.error(e)
     return {"ip": None, "location": None}
 
 """
@@ -92,3 +107,14 @@ def test(test_url,
             test_url=test_url, e=str(e)))
     r_logger.info("使用的代理：{proxies}".format(proxies=str(proxies)))
     return success_flg
+
+
+"""
+@description: 单体测试
+-------
+@param:
+-------
+@return:
+"""
+if __name__ == "__main__":
+    print(get_ip_info())
